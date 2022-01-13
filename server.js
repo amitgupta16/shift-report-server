982d325835a4be166d0a890e9e8785c1315b5e70
amitgupta16 <ryu.forever@gmail.com>
2022-01-13 19:28:03 +0530

    connection changed

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "secret",
-    database: "practice",
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
